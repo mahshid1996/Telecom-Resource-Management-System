3b43cf8a776cdc53297c07efc1510af573ef32d2
mahshid <42573509+mahshid1996@users.noreply.github.com>
2026-04-01 15:32:17 +0330

    Update emailService.js

--- notification-service/src/services/emailService.js
+++ notification-service/src/services/emailService.js
@@ -1,42 +1,29 @@
+
 const nodemailer = require('nodemailer');
 require('dotenv').config();
-const logger = require('../utils/logger'); 
-
-const createTransporter = () => {
-  return nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.GMAIL_SENDER,
-      clientId: process.env.GMAIL_CLIENT_ID,
-      clientSecret: process.env.GMAIL_CLIENT_SECRET,
-      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-      accessToken: process.env.GMAIL_ACCESS_TOKEN
-    }
-  });
-};
 
 async function sendEmail(to, body, subject = 'Notification') {
   try {
-    const transporter = createTransporter();
-        console.log(JSON.stringify( transporter.options )); // debug log
-    const test ={
-      from: `"Notif Service" <${process.env.GMAIL_SENDER}>`,
-      to,
-      subject,
-      text: body,
-    }
-        console.log(JSON.stringify( test )); // debug log
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.GMAIL_SENDER,
+        clientId: process.env.GMAIL_CLIENT_ID,
+        clientSecret: process.env.GMAIL_CLIENT_SECRET,
+        refreshToken: process.env.GMAIL_REFRESH_TOKEN
+      }
+    });
+
     const info = await transporter.sendMail({
-      from: `"Notif Service" <${process.env.GMAIL_SENDER}>`,
+      from: `"Notification Service" <${process.env.GMAIL_SENDER}>`,
       to,
       subject,
       text: body
     });
-    logger.info(`Email sent: ${info.messageId} to ${to}`);
+
     return info;
   } catch (err) {
-    logger.error(`Failed to send email to ${to}: ${err.message}`);
     throw err;
   }
 }
