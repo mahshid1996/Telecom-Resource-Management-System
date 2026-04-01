6bc56c60d41e3bff412b3bd2b84f088cb62c7e06
mahshid <42573509+mahshid1996@users.noreply.github.com>
2026-04-01 18:20:23 +0330

    Update swagger.js

--- config-service/src/swagger.js
+++ config-service/src/swagger.js
@@ -1,19 +1,20 @@
+
+const path = require('path');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
-// Swagger Settings
 const options = {
   definition: {
     openapi: '3.0.0',
     info: {
       title: 'Master Config API',
       version: '1.0.0',
-      description: 'API for managing master config resources with various filters and CRUD operations.',
+      description: 'API for managing master config resources.',
     },
   },
-  apis: ['./src/routes/masterConfigRoutes.js', './src/app.js'], // API Related Files
+  apis: [path.join(__dirname, 'routes/*.js')],
 };
 
-const specs = swaggerJsdoc(options); // Build Swagger Documentation
+const specs = swaggerJsdoc(options);
 
 module.exports = { swaggerUi, specs };
