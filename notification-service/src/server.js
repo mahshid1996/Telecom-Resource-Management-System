68c8df0ef058c8790d0893753445ac3de20b38b4
mahshid <42573509+mahshid1996@users.noreply.github.com>
2026-04-01 15:34:37 +0330

    Update server.js

--- notification-service/src/server.js
+++ notification-service/src/server.js
@@ -1,6 +1,39 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
-const config = require('../src/config/defaultConfig.json'); 
+const logger = require('./utils/logger');
+const axios = require('axios');
 
-const PORT = config.port || 3039;
+const PORT = process.env.PORT || 3039;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+async function loadNotificationConfigs(app) {
+  try {
+    const response = await axios.get(
+      'http://config-service:3030/master-config?type=NotificationConfig&status=Active'
+    );
+
+    app.set('notificationConfigs', response.data);
+    logger.info('Notification configs loaded');
+  } catch (err) {
+    logger.error('Failed to load notification configs');
+  }
+}
+
+async function start() {
+  try {
+    await mongoose.connect(MONGO_URI);
+    logger.info('MongoDB connected successfully');
+
+    await loadNotificationConfigs(app);
+
+    app.listen(PORT, () => {
+      logger.info(`Notification service running on port ${PORT}`);
+    });
+  } catch (err) {
+    logger.error(`Startup failed: ${err.message}`);
+    process.exit(1);
+  }
+}
+
+start();
