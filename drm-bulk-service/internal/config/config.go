a4daa12ecce9b34afa5f6a698150381646b1738b
mahshid <42573509+mahshid1996@users.noreply.github.com>
2026-04-01 17:54:26 +0330

    Update config.go

--- drm-bulk-service/internal/config/config.go
+++ drm-bulk-service/internal/config/config.go
@@ -3,22 +3,22 @@ package config
 import "os"
 
 type Config struct {
-	Port     string
-	MongoURI string
-	MongoDB  string
+  Port     string
+  MongoURI string
+  MongoDB  string
 }
 
 func Load() Config {
-	return Config{
-		Port:     getEnv("PORT", "3035"),
-		MongoURI: getEnv("MONGO_URI", "mongodb://localhost:27017"),
-		MongoDB:  getEnv("MONGO_DB", "resourceDB"),
-	}
+  return Config{
+    Port:     getEnv("PORT", "3035"),
+    MongoURI: getEnv("MONGO_URI", "mongodb://localhost:27017/resourceDB"),
+    MongoDB:  getEnv("MONGO_DB", "resourceDB"),
+  }
 }
 
 func getEnv(key, defaultVal string) string {
-	if val, ok := os.LookupEnv(key); ok {
-		return val
-	}
-	return defaultVal
+  if val, ok := os.LookupEnv(key); ok {
+    return val
+  }
+  return defaultVal
 }
